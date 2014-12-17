dee355e05e2137f448da6606c8556cc0a597b997
Jean-Francois <jean.francois.garreau@gmail.com>
2014-12-17 11:18:01 +0100

    first commit

---
+++ app/src/main/java/com/binomed/jef/udacityapp/NewDetailFragment.java
@@ -0,0 +1,62 @@
+package com.binomed.jef.udacityapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.binomed.jef.udacityapp.dummy.DummyContent;
+
+/**
+ * A fragment representing a single New detail screen.
+ * This fragment is either contained in a {@link NewListActivity}
+ * in two-pane mode (on tablets) or a {@link NewDetailActivity}
+ * on handsets.
+ */
+public class NewDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public NewDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_new_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.new_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+}
